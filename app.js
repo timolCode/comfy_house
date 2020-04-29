5538dc07f250178bf8cde7edbbd6678e691b5039
OLANIYI, Timothy <58417224+timolCode@users.noreply.github.com>
2020-04-29 10:13:29 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+const cartBtn = document.querySelector(".cart-btn");
+const closeCartBtn = document.querySelector(".close-cart");
+const clearCartBtn = document.querySelector(".clear-cart");
+const cartDOM = document.querySelector(".cart");
+const cartOverly = document.querySelector(".cart-overly");
+const cartItems = document.querySelector("cart-items");
+const cartTotal = document.querySelector(".cart-total");
+const cartContent = document.querySelector(".cart-content");
+const productsDOM = document.querySelector("products-center");
+
+// cart
+let cart =[];
+
+//getting the products
+class products {
+
+}
+//display produts
+class UI {
+
+}
+//local storage
+class Storage {
+
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const ui = new UI();
+    const products = new products();
+});
